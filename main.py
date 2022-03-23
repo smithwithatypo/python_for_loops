fd857aebf4427435a72d33a8355935552b49ca83
Stephen <51218665+smithwithatypo@users.noreply.github.com>
2022-03-23 00:25:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+
+# For loops
+# for number in range(0,4):
+#   print(number)
+
+
+
+#########   For-loops with arrays   ##########
+
+# list1 = [5, 20, 3, 11]
+# print(list1, "\n")
+
+# for item in list1:
+#   print(item)
+
+
+
+#########   For-loops with indexes   ##########
+
+list2 = ["milk", "eggs", "bread"]
+print(f"Grocery List: {list2} \n")
+
+for index, item in enumerate(list2):
+  print(f"Index: {index} \n Item: {item} \n")
+
+
+
+
+#########   Why use for-loops with indexes?   ##########
+
+list2 = ["milk", "eggs", "bread"]
+print(f"Grocery List: {list2} \n")
+
+# TODO: add conditional if statement to change item in the list
+for index, item in enumerate(list2):
+  print(f"Index: {index} \n Item: {item} \n")
